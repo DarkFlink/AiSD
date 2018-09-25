576f027e98aab0af0ddeddc8547467b3b2ec334c
DarkFlink <zond.999@yandex.ru>
2018-09-25 08:53:52 +0300

    main.c reworked

--- 7382/GosudarkinYS/lab2/Source/main.cpp
+++ 7382/GosudarkinYS/lab2/Source/main.cpp
@@ -1,34 +1,111 @@
-#include <vector>
 #include <iostream>
-#include <stack>
 #include <cstdlib>
 
 using namespace std;
+typedef unsigned long ul;
 
 bool Brackets(string &str);
 void BracketRework(string &str);
 
+template<class Type>
+class listElement {
+public:
+    listElement* next;
+    listElement* prev;
+    Type liter;
+};
+
+template<class Type>
+class listStack {
+public:
+    listStack() {
+        mSize = 0;
+    }
+    ~listStack() {
+        while(!empty())
+            pop_Back();
+    }
+
+    void push_Back(Type &data){ // Вставка в конец
+        listElement<Type>* ptr = new listElement<Type>;
+        if(!mSize) {
+            root = ptr;
+            end = ptr;
+            ptr->next = NULL;
+            ptr->prev = NULL;
+        } else {
+            ptr->next = NULL;
+            ptr->prev = end;
+            end->next = ptr;
+            end = ptr;
+        }
+        ptr->liter = data;
+        mSize++;
+    }
+
+    void pop_Back(){ // Удаление
+        if(!mSize)
+            error_Handler(1);
+        else if(mSize == 1) {
+            delete root;
+            root = NULL;
+            end = NULL;
+        } else {
+            end = end->prev;
+            delete end->next;
+            end->next = NULL;
+        }
+        mSize--;
+    }
+
+    char top(){
+        if(!mSize)
+            error_Handler(2);
+        return end->liter;
+    }
+
+    bool empty(){
+        if(!mSize)
+            return true;
+        else
+            return false;
+    }
+
+private:
+
+    void error_Handler(ul descriptor){
+        if(descriptor == 1) {
+            cerr << "[ERROR] Trying to pop() in empty stack" << endl;
+            exit(1);
+        } else if(descriptor == 2) {
+            cerr << "[ERROR] Trying to top() in empty stack" << endl;
+            exit(1);
+        }
+    }
+
+    ul mSize;
+    listElement<Type>* root;
+    listElement<Type>* end;
+};
+
 int main() {
     string str1;
     string str2;
-    cout << "Enter 1-st string" << endl;
+    cout << "Enter 1-st string: ";
     cin >> str1;
-    cout << "Enter 2-nd string" << endl;
+    cout << "Enter 2-nd string: ";
     cin >> str2;
 
-    if(!Brackets(str1)) {
-        cerr << "[ERROR] 1-st string isn't hierarchial list. Aborting!" << endl;
+    if(!Brackets(str1) || !Brackets(str2)) {
+        cerr << "[ERROR] One of strings isn't brackets. Aborting!" << endl;
         exit(1);
-    } else if(!Brackets(str2)) {
-        cerr << "[ERROR] 2-nd string isn't hierarchial list. Aborting!" << endl;
-        exit(1);      
     }
 
     BracketRework(str1);
     BracketRework(str2);
 
-    cout << "Bracket structure [str1]:  " << str1 << endl;
-    cout << "Bracket structure [str2]:  " << str2 << endl;
+    cout << "[LOG] Bracket structure [str1]:  " << str1 << endl;
+    cout << "[LOG] Bracket structure [str2]:  " << str2 << endl;
 
     if(str1 != str2)
         cerr << "Strings !Equal" << endl;
@@ -39,7 +116,7 @@ int main() {
 }
 
 bool Brackets(string &str) {
-        stack<char> mStack;
+        listStack<char> mStack;
         if(str[0] != '(')
             return false;
 
@@ -51,10 +128,11 @@ bool Brackets(string &str) {
                     return true;
             }
 
+            char c = '(';
             if(str[it] == '(')
-                mStack.push('(');
+                mStack.push_Back(c);
             else if(str[it] == ')' && !mStack.empty()) {
-                mStack.pop();
+                mStack.pop_Back();
                 if(it != str.size()-1 && mStack.empty())
                     return false;
             }
