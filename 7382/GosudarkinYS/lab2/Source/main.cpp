e018391f6f187840f967c0a7795252a5deeffa10
DarkFlink <zond.999@yandex.ru>
2018-10-05 07:36:43 +0300

    lab2 reworked

--- 7382/GosudarkinYS/lab2/Source/main.cpp
+++ 7382/GosudarkinYS/lab2/Source/main.cpp
@@ -1,28 +1,33 @@
-#include <iostream>
-#include <vector>
-#include "binary_tree.h"
+#include "hier_list.h"
 
 using namespace std;
+using namespace h_list;
 
-int main()
-{
+int main (){
+
+    lisp _hierList1;
+    lisp _hierList2;
     string str1;
     string str2;
-    cin >> str1;
-    cin >> str2;
 
-    binary_tree<char> mTree1(str1);
-    binary_tree<char> mTree2(str2);
-    mTree1.to_structure_exp(str1);
-    mTree2.to_structure_exp(str2);
+    cout << "list1: ";
+    read_lisp(_hierList1);
+    write_lisp(_hierList1);
 
-    cout << "[OUT] Binary tree 1 s_expression: " << str1 << '\n';
-    cout << "[OUT] Binary tree 2 s_expression: " << str2 << '\n';
+    cout << "\nlist2: ";
+    read_lisp(_hierList2);
+    write_lisp(_hierList2);
+
+    tostr_hierlist(_hierList1, str1);
+    tostr_hierlist(_hierList2, str2);
+
     if(str1 == str2)
-        cout << "[OUT] Binary trees are stucture identical." << '\n';
+        cout << "\nLists 1 && 2 are structure identical\n";
     else
-        cout << "[OUT] Binary trees are NOT stucture identical." << '\n';
-    cout << "__________________________________________________\n";
+        cout << "\nLists 1 && 2 are NOT structure identical\n";
+
+    destroy(_hierList1);
+    destroy(_hierList2);
 
     return 0;
 }
